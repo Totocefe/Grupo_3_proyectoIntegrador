4f43529dfb9ed28eac459382204a1bc3e914ec9c
totocefe <totocefe@hotmail.com>
2022-05-17 15:49:14 -0300

    subiendo las carpetas y archivos nuevos

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const path = require('path');
+const PORT = process.env.PORT || 3000; 
+const app = express();
+
+const publicPath= path.resolve(__dirname, './public');
+app.use(express.static(publicPath));
+
+app.listen(PORT,()=> {console.log(`Servidor corriendo en el puerto ${PORT}`);});
+app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))});
