781134a4fc00ce3d2c3ffac7905617f3aeed5723
totocefe <totocefe@hotmail.com>
2022-05-20 17:30:20 -0300

    archivos html agregados

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ app.use(express.static(publicPath));
 
 app.listen(PORT,()=> {console.log(`Servidor corriendo en el puerto ${PORT}`);});
 app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))});
+app.get('/register.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/register.html'))});
+app.get('/login.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/login.html'))}); 
+app.get('/productDetail.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))});
+app.get('/productCard.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/productCard.html'))});       
