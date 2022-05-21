aa32ce6194671c035046467d276995c7f8385e77
totocefe <totocefe@hotmail.com>
2022-05-21 19:33:57 -0300

    subiendo  productDetail

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.listen(PORT,()=> {console.log(`Servidor corriendo en el puerto ${PORT}`);});
-app.get('/',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))});
+app.get('/home.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home.html'))});
 app.get('/register.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/register.html'))});
 app.get('/login.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/login.html'))}); 
 app.get('/productDetail.html',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/productDetail.html'))});
