b2e9d17dfeaa5c05d4cd7a9c4249427c43b40765
totocefe <totocefe@hotmail.com>
2022-06-03 13:13:30 -0300

    include solucionado

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const app = express();
 const publicPath= path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
-app.set('view engines', 'ejs');
+app.set('view engine', 'ejs');
 app.set('views',path.join(__dirname, 'views'));
 
 app.listen(PORT,()=> {console.log(`Servidor corriendo en el puerto ${PORT}`);});
-app.get('/home',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/home'))});
-app.get('/register',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/register'))});
-app.get('/login',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/login'))}); 
-app.get('/productDetail',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/productDetail'))});
-app.get('/productCart',(req,res)=>{res.sendFile(path.resolve(__dirname,'./views/productCart'))});       
+app.get('/',(req,res)=>{res.render(path.resolve(__dirname,'./views/home'))});
+app.get('/register',(req,res)=>{res.render(path.resolve(__dirname,'./views/register'))});
+app.get('/login',(req,res)=>{res.render(path.resolve(__dirname,'./views/login'))}); 
+app.get('/productDetail',(req,res)=>{res.render(path.resolve(__dirname,'./views/productDetail'))});
+app.get('/productCart',(req,res)=>{res.render(path.resolve(__dirname,'./views/productCart'))});       
